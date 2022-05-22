d0f6dd22f9af83d0d8e774f0d0f2a1d16ec93444
RaiderKnight <raiderknight2006@hotmail.com>
2022-05-22 18:44:12 -0400

    CleanUp

--- _pack.cmd
+++ _pack.cmd
@@ -1,4 +1,4 @@
-set target=RKModManager.zip
+set target=RKLauncher.zip
 set SEVENZIP=%programfiles%\7-Zip\7z.exe
 IF EXIST %target% del %target%
 "%SEVENZIP%" a %target% .\readme.txt .\bin\Release\RKLauncher.exe
